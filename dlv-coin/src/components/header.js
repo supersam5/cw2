c4751a44ede200b7b3591226d8f9be82664303a6
Samuel Egemba <egembasam@gmail.com>
2023-04-03 15:19:48 +0100

    UI Changes

--- dlv-coin/src/components/header.js
+++ dlv-coin/src/components/header.js
@@ -1,5 +1,6 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
@@ -12,18 +13,16 @@ function Header() {
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Status</a></li>
+          <li><Link to='/'><a href="#" class="nav-link px-2 text-secondary">Home</a></Link></li>
+          <li><Link to="/status"><a href="#" class="nav-link px-2 text-white">Status</a></Link></li>
           
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search" spellcheck="false" data-ms-editor="true"/>
-        </form>
+        
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <Link to="/register"><button type="button" class="btn btn-outline-light me-2">Login</button></Link>
+          <Link to="/register"><button type="button" class="btn btn-warning">Sign-up</button></Link>
         </div>
       </div>
     </div>
--- dlv-coin/src/components/orderstatus.js
+++ dlv-coin/src/components/orderstatus.js
@@ -13,22 +13,32 @@ function OrderTracking() {
     <div class="order-status">
   <div class="status-item completed">
     <div class="status-circle"></div>
+    <div class="status-label">Available</div>
+  </div>
+  <div class="status-line"></div>
+  <div class="status-item completed">
+    <div class="status-circle"></div>
     <div class="status-label">Order Placed</div>
   </div>
   <div class="status-line"></div>
   <div class="status-item active">
     <div class="status-circle"></div>
-    <div class="status-label">Processing</div>
+    <div class="status-label">Order Received</div>
   </div>
   <div class="status-line"></div>
   <div class="status-item">
     <div class="status-circle"></div>
-    <div class="status-label">Shipped</div>
+    <div class="status-label">Item Dispatched</div>
   </div>
   <div class="status-line"></div>
   <div class="status-item">
     <div class="status-circle"></div>
-    <div class="status-label">Delivered</div>
+    <div class="status-label">Delayed</div>
+  </div>
+  <div class="status-line"></div>
+  <div class="status-item">
+    <div class="status-circle"></div>
+    <div class="status-label">Delayed</div>
   </div>
 </div>
 
